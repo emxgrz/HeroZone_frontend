688d7db7b6b192fc9fa21962af7b4f40cba020d5
clara seijo <clasei@proton.me>
2024-09-30 22:23:33 +0200

    SuperHeroList.jsx 1st setup done

--- src/components/SuperHeroList.jsx
+++ src/components/SuperHeroList.jsx
@@ -1,6 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import SuperHeroCard from "./SuperHeroCard";
+
 function SuperHeroList() {
+
+  const [ marvelSuperheroes, setMarvelSuperheroes ] = useState([])
+
+  useEffect(() => {
+    const fetchMarvelSuperheroes = async () => {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/v1/public/characters`, {
+          params: {
+            apikey: import.meta.env.VITE_API_KEY,
+            ts: import.meta.env.VITE_TIMESTAMP,
+            hash: import.meta.env.VITE_HASH
+            // limit: 42
+          }
+        })
+        
+        setMarvelSuperheroes(response.data.data.results)
+
+      } catch (error) {
+        console.log('Error fetching Marvel SuperHeroes: ', error)
+      }
+    }
+
+    fetchMarvelSuperheroes()
+
+  }, [])
+
+
   return (
-    <div>SuperHeroList</div>
+    <div className="marvel-superhero-list">
+      {marvelSuperheroes.map((marvelSuperhero) => (
+        <SuperHeroCard
+        key={marvelSuperhero.id}
+        marvelSuperhero={marvelSuperhero}
+        />
+      ))}
+    </div>
   )
 }
 
