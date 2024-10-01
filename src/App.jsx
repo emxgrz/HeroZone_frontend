ef2fc8f73690b9a173c4eb627e8651275d228103
clara seijo <clasei@proton.me>
2024-10-01 11:59:43 +0200

    App + main work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+
+import Home from './pages/Home.jsx'
+import NewSuperHeroes from './pages/NewSuperHeroes.jsx'
 
 function App() {
 
 
   return (
-    <>
+    <div className='app-page'>
 
-    </>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/new-superheroes" element={<NewSuperHeroes />} />
+
+          {/* <Route path="/custom-superhero/:id" element={<Edit... />} />
+          <Route path="/new-superhero/:id" element={<Create... />} /> */}
+    
+        </Routes>
+
+        <Footer />
+
+    </div>
   )
 }
 
